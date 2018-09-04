b4436c7580668f9c8b2a4e1cd0034f336a39edda
silencecynic <vorjohnl@gmail.com>
2018-09-04 22:29:58 +0800

    "--"

--- ridiculous/config/api-config/src/main/java/com/api/server/config/Configuration.java
+++ ridiculous/config/api-config/src/main/java/com/api/server/config/Configuration.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+
 @EnableAspectJAutoProxy (proxyTargetClass = true , exposeProxy = true)
 @SpringBootApplication (scanBasePackages = {"com.api.server.config"})
-public class ServerConfig {
+public class Configuration {
   public static void main(String[] args) {
-    SpringApplication.run(ServerConfig.class,args);
+    SpringApplication.run(Configuration.class,args);
   }
 }
