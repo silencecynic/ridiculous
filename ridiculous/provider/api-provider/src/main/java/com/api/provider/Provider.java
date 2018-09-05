792a963096689c7fb9e280e3117bb5a45688a4ec
vonslience <vorjohnl@gmail.com>
2018-09-05 13:00:17 +0800

    "pom.xml modify"

--- ridiculous/provider/api-provider/src/main/java/com/api/provider/Provider.java
+++ ridiculous/provider/api-provider/src/main/java/com/api/provider/Provider.java
@@ -3,11 +3,9 @@ package com.api.provider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-@EnableAspectJAutoProxy (proxyTargetClass = true, exposeProxy = true)
-@SpringBootApplication (scanBasePackages = {"com.api.provider"})
 @EnableDiscoveryClient
+@SpringBootApplication (scanBasePackages = {"com.api.provider"})
 public class Provider {
     public static void main(String[] args) {
         SpringApplication.run(Provider.class,args);
--- ridiculous/provider/api-provider/src/main/java/com/api/provider/handler/DomainHandler.java
+++ ridiculous/provider/api-provider/src/main/java/com/api/provider/handler/DomainHandler.java
@@ -3,6 +3,7 @@ package com.api.provider.handler;
 import com.api.commons.http.HttpModel;
 import com.api.provider.service.DomainService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,11 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DomainHandler {
 
+    private DiscoveryClient discoveryClient;
+
     private DomainService domainService;
 
     @Autowired
-    public DomainHandler (DomainService domainService) {
+    public DomainHandler (DomainService domainService, DiscoveryClient discoveryClient) {
         this.domainService = domainService;
+        this.discoveryClient = discoveryClient;
     }
 
     @GetMapping (value = "/domain/{id}",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
@@ -24,6 +28,9 @@ public class DomainHandler {
         return domainService.get();
     }
 
-
+    @GetMapping (value = "/discovery/{id}",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public HttpModel getDiscovery() {
+        return HttpModel.build().setResponse(discoveryClient.getServices());
+    }
 
 }
