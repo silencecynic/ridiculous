04e5023b00d386b51ad2e4c25f3e0f5e1888a473
vonslience <vorjohnl@gmail.com>
2018-09-19 11:37:14 +0800

    "pom.xml modify"

--- ridiculous/provider/api-provider/src/main/java/com/api/provider/handler/DomainHandler.java
+++ ridiculous/provider/api-provider/src/main/java/com/api/provider/handler/DomainHandler.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RequestMapping (value = "/api/v1")
 @RestController
 public class DomainHandler {
